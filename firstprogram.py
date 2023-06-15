e45c222fcf2c6085ef105dab9857a992515cd600
tiarakabra <136440241+tiarakabra@users.noreply.github.com>
2023-06-15 13:06:50 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,14 @@
+print("Hello World")
+#This is a comment
+a = 10
+print("Print a =",a)
+name = input("Enter your name: ")
+print(name)
+mark1 = int(input("Enter your marks: "))
+print(mark1)
+mark2 = int(input("Enter your marks: "))
+print(mark2)
+mark3 = int(input("Enter your marks: "))
+print(mark3)
+average = mark1+mark2+mark3/3
+print("This is Average =",average)
